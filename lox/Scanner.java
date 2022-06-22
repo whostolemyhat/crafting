f7483a81d2e2c39290118667a5b068bbf8394d93
James Baum <james@jamesbaum.co.uk>
2022-06-22 16:18:29 +0100

    Chapter 4 scanner

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -8,6 +8,27 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = -1;
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",   AND);
+    keywords.put("class", CLASS);
+    keywords.put("else",  ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for",   FOR);
+    keywords.put("fun",   FUN);
+    keywords.put("if",    IF);
+    keywords.put("nil",   NIL);
+    keywords.put("or",    OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this",  THIS);
+    keywords.put("true",  TRUE);
+    keywords.put("var",   VAR);
+    keywords.put("while", WHILE);
+  }
   
   Scanner(String source) {
     this.source = source;
@@ -94,7 +115,13 @@ class Scanner {
         break;
 
       default:
-        Lox.error(line, "Unexpected character");
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character");
+        }
         break;
     }
   }
@@ -116,5 +143,75 @@ class Scanner {
       return '\0';
     }
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) {
+      return '\0';
+    }
+    return source.charAt(current + 1);
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') {
+        line++;
+        advance();
+      }
+      
+      if (isAtEnd()) {
+        Lox.error(line, "unterminated string");
+        return;
+      }
+
+      // closing '"'
+      advance();
+
+      // trim quotes
+      String value = source.substring(start + 1, current - 1);
+      addToken(STRING, value);
+    }
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+          (c >= 'A' && c <= 'Z') ||
+          c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private void number() {
+    while (isDigit(peek())) {
+      advance();
+    }
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance();
+
+      while (isDigit(peek())) {
+        advance();
+      }
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) {
+      advance();
+    }
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) {
+      type = IDENTIFIER;
+    }
+    addToken(type);
   }
 }
