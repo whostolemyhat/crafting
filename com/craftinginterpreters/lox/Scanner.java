344dbdb7e5e58bce0ac157b5ddc9fae693d96bea
James Baum <james@jamesbaum.co.uk>
2022-06-23 09:57:22 +0100

    Fix import and VS settings

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -1,6 +1,11 @@
 package com.craftinginterpreters.lox;
 
-import static craftinginterpreters.lox.TokenType.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.craftinginterpreters.lox.TokenType.*;
 
 class Scanner {
   private final String source;
